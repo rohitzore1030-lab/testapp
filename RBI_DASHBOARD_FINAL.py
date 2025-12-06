a5b3726beb1805275c88b645f057c613969d52d9
rohitzore1030-lab <rohitzore1030@gmail.com>
2025-12-06 15:14:57 +0530

    Fix import case for seaborn in RBI_DASHBOARD_FINAL.py

--- RBI_DASHBOARD_FINAL.py
+++ RBI_DASHBOARD_FINAL.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import datetime
-import Seaborn as sns
+import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 
